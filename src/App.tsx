51487f611fdc057fc48425b614efb6afe2940c97
佐藤昭文 <akfm.sato@gmail.com>
2020-05-30 21:43:26 +0900

    deno update on 1.0.3, fix import map

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // @deno-types="https://deno.land/x/types/react/v16.13.1/react.d.ts"
-import React from 'https://dev.jspm.io/react@16.13.1'
+import React from 'React'
 
 const App: React.FC = () => {
   const [count, setCount] = React.useState(0)
